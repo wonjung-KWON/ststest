3c4b40b533a062647307998e22b342e61837738e
권원중 <126855396+wonjung-KWON@users.noreply.github.com>
2023-07-24 09:35:08 +0900

    first

---
+++ ststest/src/main/java/com/goodee/ststest/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.goodee.ststest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "HELLO";
+	}
+	
+}
